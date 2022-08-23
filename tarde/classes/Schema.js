cc4d80ce845466630899dad5806abe6f770e7428
Felixlestaal3 <111828446+Felixlestaal3@users.noreply.github.com>
2022-08-22 19:09:56 -0700

    Delete tarde directory

--- tarde/classes/Schema.js
+++
@@ -1,9 +0,0 @@
-class Schema {
-    constructor() {
-        //Revisen bien el puerto en el que se enciende su API :3
-        this.paypal = "http://localhost:3000/api/paypal-order/";
-        this.productos = "http://localhost:3000/api/products/";
-    }
-}
-
-
